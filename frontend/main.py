720ef256ba8cdd6ad8038d9c3e6910ae529b6984
franzmaliszt <mabulut@eskisehir.edu.tr>
2023-11-03 11:06:14 +0300

    Enable image annotation, improve layout

--- frontend/main.py
+++ frontend/main.py
@@ -3,6 +3,7 @@ import os
 import httpx
 import gradio as gr
 from dotenv import load_dotenv
+import numpy as np
 
 # from decord import VideoReader
 
@@ -12,13 +13,9 @@ BACKEND_PORT = os.environ["BACKEND_PORT"]
 FRONTEND_PORT = int(os.environ["FRONTEND_PORT"])
 
 
-def on_upload_frames(paths: set[str], evt: gr.EventData):
+def on_upload(paths: set[str], evt: gr.EventData):
     new_paths = {img["orig_name"]: img["name"] for img in evt._data}
     paths.update(new_paths)
-
-
-def on_upload_video(paths: dict[str, str], evt: gr.EventData):
-    paths.update({evt._data["orig_name"]: evt._data["name"]})
 
 
 def upload_success(paths: dict[str, str]):
@@ -35,23 +32,47 @@ def display_video(files, evt: gr.SelectData):
             return file.name
 
 
-def display_images(files, gallery, evt: gr.SelectData):
-    in_gallery = [
-        img["name"] for img in gallery if not img["name"].split("/")[-1] == evt.value
-    ]
-    if len(in_gallery) == len(gallery):
-        for file in files:
-            if file.name.split("/")[-1] == evt.value:
-                in_gallery.append(file.name)
-                break
-    return in_gallery
+# inputs=[file_system, input_gallery], outputs=[input_gallery]
+# def display_images(files, gallery, evt: gr.SelectData):
+#     in_gallery = [
+#         img["name"] for img in gallery if not img["name"].split("/")[-1] == evt.value
+#     ]
+#     if len(in_gallery) == len(gallery):
+#         for file in files:
+#             if file.name.split("/")[-1] == evt.value:
+#                 in_gallery.append(file.name)
+#                 break
+#     return in_gallery
+
+
+def display_img(selected: str, files):
+    for file in files:
+        if file.name.split("/")[-1] == selected:
+            return file.name
+
+
+def sketch(canvas, gallery):
+    # if len(gallery) == 5:
+    #     raise gr.Error("Can't have more than 5 frames.")
+    in_gallery = [img["name"] for img in gallery]
+
+    nonzero_indices = np.nonzero(canvas["mask"])
+    min_x, max_x = np.min(nonzero_indices[1]), np.max(nonzero_indices[1])
+    min_y, max_y = np.min(nonzero_indices[0]), np.max(nonzero_indices[0])
+    roi = canvas["image"][min_y:max_y, min_x:max_x]
+
+    return in_gallery + [roi]
+
+
+def undo(gallery):
+    return [img["name"] for img in gallery][:-1]
 
 
 async def submit(
     video_path: str, frame_objs: list[dict], output_size: int, sample_rate: float
 ) -> list:
-    if not 2 < len(frame_objs) < 6:
-        raise gr.Error("Please upload between 3 and 5 frames.")
+    if len(frame_objs) < 3:
+        raise gr.Error("Please upload minimum 3 frames.")
     frame_paths = [metadata["name"] for metadata in frame_objs]
     async with httpx.AsyncClient() as client:
         response = client.post(
@@ -91,61 +112,42 @@ with gr.Blocks(title="Towards Gesund") as demo:
     gr.Markdown("""# Content based image retrieval""")
     with gr.Accordion("Docs", open=False):
         gr.HTML("""<h3>To be added...</h3>""")
-    with gr.Row():
-        with gr.Column(scale=2):
-            with gr.Tab("Search"):
-                with gr.Row():
-                    in_video_dropdown = gr.Dropdown(
-                        scale=5,
-                        label="Sample video",
-                        interactive=False,
-                    )
-                    upload_video = gr.UploadButton(
-                        "Upload", variant="primary", file_types=["mp4"]
-                    )
-                with gr.Row():
-                    input_frames = gr.Dropdown(
-                        multiselect=True,
-                        max_choices=5,
-                        label="Sample frames",
-                        interactive=False,
-                        scale=5,
-                    )
-                    upload_frames = gr.UploadButton(
-                        "Upload",
-                        variant="primary",
-                        file_count="multiple",
-                        file_types=["image"],
-                    )
-                with gr.Row(variant="compact"):
-                    output_size = gr.Slider(
-                        minimum=1,
-                        maximum=100,
-                        value=40,
-                        step=1,
-                        label="Result size in images",
-                        interactive=True,
-                    )
-                    sample_rate = gr.Number(
-                        minimum=0.1,
-                        maximum=1,
-                        value=0.5,
-                        step=0.1,
-                        label="Auto-stride",
-                        interactive=True,
-                    )
-                run_btn = gr.Button(value="Run", variant="primary")
-            with gr.Tab("Files"):
-                uploaded = gr.State(dict())
-                file_system = gr.File(
-                    # value=["data/stomach-ulcer.mp4", "data/ulcer-01.jpg"],
-                    file_count="multiple",
-                    file_types=["image", "mp4"],
-                    label="File system",
-                    interactive=False,
-                )
+    with gr.Row(equal_height=False):
+        with gr.Column(variant="panel", scale=2):
+            with gr.Row():
+                with gr.Column():
+                    gr.HTML("""<h3>1. Select a video</h3>""")
+                    video_dropdown = gr.Dropdown(show_label=False, min_width=480, interactive=True)
+                    gr.HTML("""<h3>2. Select images to annotate</h3>""")
+                    image_dropdown = gr.Dropdown(show_label=False, min_width=480)
+                    gr.HTML("""<h3>3. Options</h3>""")
+                    with gr.Row():
+                        output_size = gr.Slider(
+                            minimum=1,
+                            maximum=100,
+                            value=40,
+                            step=1,
+                            label="Result size in images",
+                            interactive=True,
+                        )
+                        sample_rate = gr.Number(
+                            minimum=0.1,
+                            maximum=1,
+                            value=0.5,
+                            step=0.1,
+                            label="Auto-stride",
+                            interactive=True,
+                        )
+                with gr.Column(scale=3):
+                    # TODO: make canvas interactive=False, see issue: #5945
+                    sketch_pad = gr.Image(tool="sketch")
+                    with gr.Row(variant="panel"):
+                        annotate_btn = gr.Button(
+                            value="Confirm Annotation", variant="primary", scale=0
+                        )
+                        undo_btn = gr.Button(value="Undo", scale=0, min_width=80)
         with gr.Column(scale=1):
-            with gr.Tab("Display"):
+            with gr.Tab("Experiments"):
                 input_video = gr.Video(format="mp4", label="Video", interactive=False)
                 input_gallery = gr.Gallery(
                     label="Sample frames",
@@ -154,10 +156,11 @@ with gr.Blocks(title="Towards Gesund") as demo:
                     object_fit="contain",
                     show_download_button=False,
                 )
+                run_btn = gr.Button(value="Run", variant="primary", scale=0, min_width=160)
             with gr.Tab("Result"):
                 dataset = gr.Gallery(label="Result dataset")
-                with gr.Box():
-                    with gr.Row(variant="default", equal_height=True):
+                with gr.Group():
+                    with gr.Row(equal_height=True):
                         output_formats = gr.Dropdown(
                             ["DICOM", "NIFTI", "PNG", "JPEG", "TIFF"],
                             label="Output Format",
@@ -168,22 +171,35 @@ with gr.Blocks(title="Towards Gesund") as demo:
                         download_btn = gr.Button(
                             value="↓", variant="secondary", min_width=50
                         )
+            with gr.Accordion("Files"):
+                uploaded = gr.State(dict())
+                upload_btn = gr.UploadButton(
+                    "Upload",
+                    scale=0,
+                    file_count="multiple",
+                    file_types=["jpg", "jpeg", "png", "mp4"],
+                )
+                file_system = gr.File(
+                    file_count="multiple",
+                    file_types=["image", "mp4"],
+                    label="File system",
+                    interactive=False,
+                )
 
     demo.load(initialize, outputs=[])
-    upload_video.upload(on_upload_video, inputs=[uploaded]).success(
+    upload_btn.upload(on_upload, inputs=[uploaded]).success(
         upload_success,
         inputs=uploaded,
-        outputs=[file_system, in_video_dropdown, input_frames],
+        outputs=[file_system, video_dropdown, image_dropdown],
     )
-    upload_frames.upload(on_upload_frames, inputs=[uploaded]).success(
-        upload_success,
-        inputs=uploaded,
-        outputs=[file_system, in_video_dropdown, input_frames],
+    video_dropdown.select(display_video, inputs=file_system, outputs=[input_video])
+    image_dropdown.select(
+        display_img, inputs=[image_dropdown, file_system], outputs=[sketch_pad]
     )
-    in_video_dropdown.select(display_video, inputs=file_system, outputs=[input_video])
-    input_frames.select(
-        display_images, inputs=[file_system, input_gallery], outputs=[input_gallery]
-    )
+    annotate_btn.click(
+        sketch, inputs=[sketch_pad, input_gallery], outputs=input_gallery
+    ).then(display_img, inputs=[image_dropdown, file_system], outputs=[sketch_pad])
+    undo_btn.click(undo, inputs=input_gallery, outputs=input_gallery)
     run_btn.click(
         submit,
         inputs=[input_video, input_gallery, output_size, sample_rate],
@@ -192,5 +208,5 @@ with gr.Blocks(title="Towards Gesund") as demo:
 
 
 if __name__ == "__main__":
-    demo.launch(server_port=FRONTEND_PORT, server_name="0.0.0.0")
-    # demo.launch(debug=True, server_port=8022, server_name="0.0.0.0")
+    # demo.launch(server_port=FRONTEND_PORT, server_name="0.0.0.0")
+    demo.launch(debug=True, server_port=8022, server_name="0.0.0.0")
