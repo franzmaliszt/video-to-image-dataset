95c55d4bd21850ec1dd5f81fb784640987751a40
franzmaliszt <mabulut@eskisehir.edu.tr>
2023-11-06 11:09:08 +0300

    Establish communication to api and result backend, rearrange components

--- frontend/main.py
+++ frontend/main.py
@@ -1,11 +1,14 @@
 import os
+from asyncio import sleep
 
 import httpx
+import numpy as np
 import gradio as gr
 from dotenv import load_dotenv
-import numpy as np
+from decord import VideoReader
 
-# from decord import VideoReader
+from imgprocess import encode_img, dilute
+
 
 
 load_dotenv()
@@ -69,39 +72,46 @@ def undo(gallery):
 
 
 async def submit(
-    video_path: str, frame_objs: list[dict], output_size: int, sample_rate: float
-) -> list:
-    if len(frame_objs) < 3:
+    experiment_name: str,
+    video_path: str,
+    img_metadata: list[dict],
+    output_size: int,
+    dilution_rate: float,
+    task_ids: dict[str, str],
+):
+    if len(img_metadata) < 3:
         raise gr.Error("Please upload minimum 3 frames.")
-    frame_paths = [metadata["name"] for metadata in frame_objs]
+    img_paths = [img["name"] for img in img_metadata]
+    sample_encodings = [encode_img(path) for path in img_paths]
+    video_encodings = dilute(video_path, dilution_rate, encode=True)
+
     async with httpx.AsyncClient() as client:
-        response = client.post(
-            f"http://backend:{BACKEND_PORT}/test",
+        response = await client.post(
+            f"http://backend:{BACKEND_PORT}/test/submit",
             json={
-                "video_path": video_path,
-                "frame_paths": frame_paths,
-                "output_size": output_size,
-                "sample_rate": sample_rate,
+                "video": video_encodings,
+                "images": sample_encodings,
             },
+            params={"output_size": output_size},
         )
-        response.raise_for_status()
-        return response.json().get("result", [])
-    # return list of images
+    task_id = response.json().get("task_id")
+    task_ids.update({experiment_name: task_id})
 
 
-async def hello(text: str):
-    url = f"http://backend:{BACKEND_PORT}/test/hello{text}"
-    async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-        response.raise_for_status()
-    return response.json().get("message", "Error")
-
-    # url = f"http://backend:{BACKEND_PORT}/test/hello"
-    # data = {"text": text}
-    # async with httpx.AsyncClient() as client:
-    #     response = await client.post(url, json=data)
-    #     response.raise_for_status()
-    # return response.json().get("message", "Error")
+async def await_result(name: str, video_path: str, task_ids: dict[str, str]):
+    task_id = task_ids[name]
+    url = f"http://backend:{BACKEND_PORT}/tasks/{task_id}"
+    for _ in range(5):
+        await sleep(5)
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+        result = response.json()
+        if result["status"] == "SUCCESS":
+            break
+    indices = result["result"]["frame_indices"]
+    vr = VideoReader(video_path)
+    frames = vr.get_batch(indices).asnumpy()
+    return frames
 
 
 def initialize():
@@ -109,6 +119,8 @@ def initialize():
 
 
 with gr.Blocks(title="Towards Gesund") as demo:
+    uploaded = gr.State(dict())
+    task_ids = gr.State(dict())
     gr.Markdown("""# Content based image retrieval""")
     with gr.Accordion("Docs", open=False):
         gr.HTML("""<h3>To be added...</h3>""")
@@ -129,15 +141,15 @@ with gr.Blocks(title="Towards Gesund") as demo:
                             maximum=100,
                             value=40,
                             step=1,
-                            label="Result size in images",
+                            label="Max output",
                             interactive=True,
                         )
-                        sample_rate = gr.Number(
+                        dilution_rate = gr.Number(
                             minimum=0.1,
                             maximum=1,
-                            value=0.5,
+                            value=0.2,
                             step=0.1,
-                            label="Auto-stride",
+                            label="Frame dilution",
                             interactive=True,
                         )
                 with gr.Column(scale=3):
@@ -156,14 +168,13 @@ with gr.Blocks(title="Towards Gesund") as demo:
                     )
         with gr.Column(scale=1):
             with gr.Accordion("Files", open=False):
-                uploaded = gr.State(dict())
                 file_system = gr.File(
                     file_count="multiple",
                     file_types=["image", "mp4"],
                     label="File system",
                     interactive=False,
                 )
-            with gr.Tab("Experiments"):
+            with gr.Tab("New Project"):
                 input_video = gr.Video(format="mp4", label="Video", interactive=False)
                 input_gallery = gr.Gallery(
                     label="Sample frames",
@@ -173,26 +184,31 @@ with gr.Blocks(title="Towards Gesund") as demo:
                     show_download_button=False,
                 )
                 with gr.Row(variant="panel"):
-                    experiment_name = gr.Textbox(max_lines=1, placeholder="Experiment name", show_label=False, container=False)
+                    experiment_name = gr.Textbox(
+                        max_lines=1,
+                        placeholder="Experiment name",
+                        show_label=False,
+                        container=False,
+                    )
                     run_btn = gr.Button(
                         value="Run", variant="primary", scale=0, min_width=160
                     )
-            with gr.Tab("Result"):
+            with gr.Tab("Projects"):
+                projects = gr.Dropdown(allow_custom_value=True, label="Projects")
                 dataset = gr.Gallery(label="Result dataset")
-                with gr.Group():
-                    with gr.Row(equal_height=True):
-                        output_formats = gr.Dropdown(
-                            ["DICOM", "NIFTI", "PNG", "JPEG", "TIFF"],
-                            label="Output Format",
-                            scale=4,
-                            min_width=50,
-                            interactive=True,
-                        )
-                        download_btn = gr.Button(
-                            value="↓", variant="secondary", min_width=50
-                        )
+                with gr.Row(variant="panel"):
+                    output_formats = gr.Dropdown(
+                        ["DICOM", "NIFTI", "PNG", "JPEG", "TIFF"],
+                        label="Output Format",
+                        scale=4,
+                        min_width=50,
+                        interactive=True,
+                    )
+                    download_btn = gr.Button(
+                        value="↓", variant="secondary", min_width=50
+                    )
 
-    demo.load(initialize, outputs=[])
+    # demo.load(initialize, inputs=task_ids, outputs=[])
     upload_btn.upload(on_upload, inputs=[uploaded]).success(
         upload_success,
         inputs=uploaded,
@@ -204,15 +220,22 @@ with gr.Blocks(title="Towards Gesund") as demo:
     )
     annotate_btn.click(
         sketch, inputs=[sketch_pad, input_gallery], outputs=input_gallery
-    ).then(display_img, inputs=[image_dropdown, file_system], outputs=[sketch_pad])
+    ).success(lambda: gr.Image(value=None), outputs=sketch_pad).success(
+        display_img, inputs=[image_dropdown, file_system], outputs=[sketch_pad]
+    )
     undo_btn.click(undo, inputs=input_gallery, outputs=input_gallery)
     run_btn.click(
         submit,
-        inputs=[input_video, input_gallery, output_size, sample_rate],
-        outputs=[dataset],
-    )
+        inputs=[
+            experiment_name,
+            input_video,
+            input_gallery,
+            output_size,
+            dilution_rate,
+            task_ids,
+        ]
+    ).success(await_result, inputs=[experiment_name, input_video, task_ids], outputs=[dataset])
 
 
 if __name__ == "__main__":
-    # demo.launch(server_port=FRONTEND_PORT, server_name="0.0.0.0")
-    demo.launch(debug=True, server_port=8022, server_name="0.0.0.0")
+    demo.launch(server_port=FRONTEND_PORT, server_name="0.0.0.0")
