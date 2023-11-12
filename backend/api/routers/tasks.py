cb0c5e35b8bd49ca7611b136abc7618bbf8d8243
franzmaliszt <mabulut@eskisehir.edu.tr>
2023-11-12 18:13:40 +0300

    Minor styling

--- backend/api/routers/tasks.py
+++ backend/api/routers/tasks.py
@@ -1,11 +1,18 @@
 from fastapi import APIRouter
+from fastapi.responses import JSONResponse
 
 from ..workers.worker import worker
 
 
 tasks_router = APIRouter(prefix="/tasks")
 
+
 @tasks_router.get("/{id}")
-async def get_task(id: str):
-    task = worker.AsyncResult(id)
-    return {"task_id": task.id, "status": task.status, "result": task.result}
+async def get_status(id: str):
+    task_result = worker.AsyncResult(id)
+    result = {
+        "id": task_result.id,
+        "status": task_result.status,
+        "result": task_result.result,
+    }
+    return JSONResponse(result)
